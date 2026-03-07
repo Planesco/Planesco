c4b5033e6cf4ae0adf293fda3edeae23c2607a5b
Stef Poli <trgsmallidori@gmail.com>
2026-03-07 00:22:16 -0500

    Fixed Build

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/logo.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [{ source: "/favicon.ico", destination: "/logo.png" }];
+  },
+};
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 export default withNextIntl(nextConfig);
